a4a41aad888d4a9109497807a9743a922488a5a5
reshreshus <reshreshus@gmail.com>
2019-12-25 01:55:29 +0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Switch, Route} from 'react-router-dom';
-import {Navbar, ProductList, Details, Cart, Default, PageNotFound, Model } from './components';
 
 
 function App() {
   return (
-   
-    <React.Fragment>
-      <Navbar/>
-      <Switch>
-        <Route path="/" exact component={ProductList}/>
-        <Route path="/details" exact component={Details}/>
-        <Route path="/cart" exact component={Cart}/>
-        <Route  component={PageNotFound}/>
-      </Switch>
-      <Model />
-    </React.Fragment>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -9,13 +9,7 @@ import { ProductProvider} from './context';
 
 
 ReactDOM.render
-(
-<ProductProvider>
-    <Router>
-        <App />
-    </Router>
-</ProductProvider>
-, document.getElementById('root'));
+( <App /> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
